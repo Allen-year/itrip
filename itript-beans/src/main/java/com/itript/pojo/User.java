1cb39b4d954623379f53209dbe81e5e648309117
starkitty <star@kitty.com>
2019-11-11 11:56:32 +0800

    init master

---
+++ itript-beans/src/main/java/com/itript/pojo/User.java
@@ -0,0 +1,9 @@
+package com.itript.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+    private String userName;
+    private String usePassWord;
+}
